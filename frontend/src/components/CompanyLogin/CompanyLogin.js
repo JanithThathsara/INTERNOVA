bc2a2d18cb32bf031f3824c69f84a5554139f130
Pabodavidumina123 <Pabodhavidumina@gmail.com>
2025-09-27 23:16:02 +0530

    Add the nav bar loging page

--- frontend/src/components/CompanyLogin/CompanyLogin.js
+++ frontend/src/components/CompanyLogin/CompanyLogin.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import "./CompanyLogin.css";
+import Nav from "../Nav/Nav";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
-const Login = () => {
+
+const CompanyLogin = () => {
   const navigate = useNavigate();
   const [formData, setFormData] = useState({
     companyName: "",
@@ -20,7 +22,10 @@ const Login = () => {
     setError("");
 
     try {
-      const res = await axios.post("http://localhost:5000/users/login", formData);
+      const res = await axios.post(
+        "http://localhost:5000/users/login",
+        formData
+      );
 
       if (res.data) {
         localStorage.setItem("loggedUser", JSON.stringify(res.data)); // save user
@@ -33,36 +38,43 @@ const Login = () => {
   };
 
   return (
-    <div className="login-background">
-    <div className="login-container">
-      <h2>Company Login</h2>
-      <form onSubmit={handleSubmit} className="login-form">
-        <label>Company Name</label>
-        <input
-          type="text"
-          name="companyName"
-          value={formData.companyName}
-          onChange={handleChange}
-          required
-        />
+    <>
+      <div className="nav-div">
+        <Nav />
+      </div>
 
-        <label>Register Number</label>
-        <input
-          type="text"
-          name="companyRegisterNumber"
-          value={formData.companyRegisterNumber}
-          onChange={handleChange}
-          required
-        />
+      <div className="login-background">
+        <div className="login-container">
+          <h2>Company Login</h2>
+          <form onSubmit={handleSubmit} className="login-form">
+            <label>Company Name</label>
+            <input
+              type="text"
+              name="companyName"
+              value={formData.companyName}
+              onChange={handleChange}
+              required
+            />
 
-        {error && <p className="error">{error}</p>}
+            <label>Register Number</label>
+            <input
+              type="text"
+              name="companyRegisterNumber"
+              value={formData.companyRegisterNumber}
+              onChange={handleChange}
+              required
+            />
 
-        <button type="submit" className="login-btn">Login</button>
-  
-      </form>
-    </div>
-    </div>
+            {error && <p className="error">{error}</p>}
+
+            <button type="submit" className="login-btn">
+              Login
+            </button>
+          </form>
+        </div>
+      </div>
+    </>
   );
 };
 
-export default Login;
+export default CompanyLogin;
